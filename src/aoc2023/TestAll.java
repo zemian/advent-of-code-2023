4210fe982a2a6a3e8ba8be0b940a3f84a525da19
Zemian Deng <zemiandeng@gmail.com>
2023-12-11 12:17:24 -0500

    Add TestAll

---
+++ src/aoc2023/TestAll.java
@@ -0,0 +1,23 @@
+package aoc2023;
+
+public class TestAll {
+    public static void main(String[] args) throws Exception {
+        var testArgs = new String[] {"test"};
+        Day1.main(testArgs);
+        Day1b.main(testArgs);
+        Day2.main(testArgs);
+        Day2b.main(testArgs);
+        Day3.main(testArgs);
+        Day3b.main(testArgs);
+        Day4.main(testArgs);
+        Day4b.main(testArgs);
+        Day5.main(testArgs);
+//        Day5b.main(testArgs);
+        Day6.main(testArgs);
+        Day6b.main(testArgs);
+        Day7.main(testArgs);
+        Day7b.main(testArgs);
+        Day8.main(testArgs);
+//        Day8b.main(testArgs);
+    }
+}

92ae1a70e7e5107d43014fcdae83d562eda9da63
Zemian Deng <zemiandeng@gmail.com>
2023-12-18 07:14:04 -0500

    Add DayTemplate

---
+++ src/aoc2023/DayTemplate.java
@@ -0,0 +1,40 @@
+package aoc2023;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import static aoc2023.TestUtils.assertEquals;
+import static java.util.function.Predicate.not;
+
+public class DayTemplate {
+    public static void main(String[] args) throws Exception {
+        var program = new DayTemplate();
+        if (args.length > 0 && args[0].equals("test")) {
+            program.runTests();
+            System.out.println("Tests passed.");
+        } else {
+            program.runMain("src/aoc2023/DayTemplate-input2.txt");
+        }
+    }
+
+    private Integer runMain(String fileName) throws Exception {
+        System.out.println("Processing input: " + fileName);
+        try (var reader = new BufferedReader(new FileReader(fileName))) {
+            reader.lines().filter(not(String::isEmpty)).forEach(line -> {
+                // Process Line
+                System.out.println(line);
+            });
+        }
+
+        return 0;
+    }
+
+    private void runTests() throws Exception {
+        testMain();
+    }
+
+    private void testMain() throws Exception {
+        Integer sum = runMain("src/aoc2023/DayTemplate-input2.txt");
+        assertEquals(sum, 0);
+    }
+}

5c4d5d8810ac328486c8af97f612816d177ab5a4
Zemian Deng <zemiandeng@gmail.com>
2023-12-24 10:21:21 -0500

    Cleanup template

--- src/aoc2023/DayTemplate.java
+++ src/aoc2023/DayTemplate.java
@@ -1,10 +1,6 @@
 package aoc2023;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-
 import static aoc2023.Utils.assertEquals;
-import static java.util.function.Predicate.not;
 
 public class DayTemplate {
     public static void main(String[] args) throws Exception {
@@ -19,12 +15,8 @@ public class DayTemplate {
 
     private Integer runMain(String fileName) throws Exception {
         System.out.println("Processing input: " + fileName);
-        try (var reader = new BufferedReader(new FileReader(fileName))) {
-            var lines = reader.lines().filter(not(String::isEmpty)).toList();
-            System.out.println(lines);
-        }
-
-        return 0;
+        var lines = Utils.readLines(fileName);
+        return lines.size();
     }
 
     private void runTests() throws Exception {
@@ -32,7 +24,7 @@ public class DayTemplate {
     }
 
     private void testMain() throws Exception {
-        Integer sum = runMain("src/aoc2023/DayTemplate-input2.txt");
+        Integer sum = runMain("src/aoc2023/DayTemplate-input1.txt");
         assertEquals(sum, 0);
     }
 }
